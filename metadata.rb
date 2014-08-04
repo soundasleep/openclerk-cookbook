a416895f198c0cd38c36e44a056c92840a7b8c48
Jevon Wright <jevon@jevon.org>
2014-08-04 16:56:08 +1200

    Enable PHP on Apache2; copy over phpinfo.php

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'jevon@jevon.org'
 license          'All rights reserved'
 description      'Installs/Configures openclerk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "apache2"
 depends "mysql", "5.3.6"
+depends "php"
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,14 @@
 include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "mysql::server"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "apache2::mod_php5"
 
 apache_site "default" do
   enable true
+end
+
+cookbook_file "phpinfo.php" do
+  path "/var/www"
 end

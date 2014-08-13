a998b74494408b0abd886a558e3d8b369b4966a3
Jevon Wright <jevon@jevon.org>
2014-08-13 17:09:35 +1200

    Clean up config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default["openclerk"]["database"] = "clerk"
 default["openclerk"]["db_username"] = "clerk"
 default["openclerk"]["path"] = "/var/www/openclerk"
-default["openclerk"]["server_name"] = "localhost.openclerk.org"
+default["openclerk"]["server_name"] = "localhost"
+default["openclerk"]["server_remote_name"] = "localhost:80"

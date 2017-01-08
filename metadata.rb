2a721beccda7e79bd018749fedeff51cfa9b0afc
Jevon Wright <soundasleep@gmail.com>
2017-01-08 15:47:11 +0000

    Depend on latest MySQL recipe
    I am hoping that by updating the dependency in the metadata, we can stop
    getting the 'You must supply a name when declaring a package resource'
    error when trying to run chef-solo.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'jevon@jevon.org'
 license          'All rights reserved'
 description      'Installs/Configures openclerk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.16'
+version          '0.2.1'
 
 depends "apache2"
-depends "mysql", "5.3.6"
+depends "mysql", '~> 8.0'
 depends "php"
 depends "database"
 depends "composer"
